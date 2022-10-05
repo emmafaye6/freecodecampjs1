9d8c5bc2f784d089b5a66583999c31df965840a1
Emma <111111773+emmafaye6@users.noreply.github.com>
2022-10-05 16:29:20 +0200

    changed ugly colors for pastel colors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["#FFF5E4", "lightcoral", "rgb(255, 209, 209)", "#FF9494"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
